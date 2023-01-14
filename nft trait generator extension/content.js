fb45ef61a6ca49cdffdb35f15180de8f9b4b66f7
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2023-01-14 13:03:47 -0500

    Stable version 0
    At this point the extension fill the opensea fields manually

--- nft trait generator extension/content.js
+++ nft trait generator extension/content.js
@@ -1,13 +1,22 @@
+
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         //https://www.whitehouse.gov/contact/
         try {
-            document.getElementById("form_20_FirstName").value = request.name;
-            document.getElementById("form_20_middle_name").value = request.middleName;
-            sendResponse({status: "Success!"});
+            var t = document.querySelector('table');
+            var htmlTableList = t.getElementsByTagName("tr");
+            var noOfRowsOnTableClient = (htmlTableList.length)-1;
+
+            for (var i = 0; i < noOfRowsOnTableClient; i++) {
+                htmlTableList[i+1].querySelector('input[placeholder="Character"]').value = request.type;
+                htmlTableList[i+1].querySelector('input[placeholder="Male"]').value = request.name;
+                console.log('hey! 1: '+i);
+            };
+            sendResponse({ status: "Success!" });
         } catch (error) {
             console.log(error)
-            sendResponse({status: "Exception occurred!"});
+            sendResponse({ status: "Exception occurred!" });
         }
     }
 );
